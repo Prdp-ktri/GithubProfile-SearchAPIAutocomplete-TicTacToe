476cbbdf11199479000c7eaf1ed4cd115e8eb4d9
Pradeep Khatri <pradeepkhatri44@live.com>
2025-02-12 14:53:23 +0530

    committing App.js
    committing App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import GithubProfileFinder from "./components/Github-Profile";
+import SearchAutocomplete from "./components/Github-Profile/Search-Autocomplete-with-API";
+
+export default function MenuList({ list = [] }) {
+  return (
+    // <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} />
+    // <TabTest />
+    // <ModalTest />
+    // <GithubProfileFinder />
+    // <SearchAutocomplete/>
+    <TicTacToe />
+  );
+}
