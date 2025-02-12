1ade8ce022cff235ec30496f618feb0b265bbe9a
Pradeep Khatri <pradeepkhatri44@live.com>
2025-02-12 14:52:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import GithubProfileFinder from "./components/Github-Profile";
+import SearchAutocomplete from "./components/Github-Profile/Search-Autocomplete-with-API";
+import TicTacToe from "./components/Github-Profile/tic-tac-toe";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -25,12 +28,14 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <GithubProfileFinder />
+      {/* <GithubProfileFinder /> */}
+      {/* <SearchAutocomplete /> */}
+      <TicTacToe />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
